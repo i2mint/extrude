c99e170f497bc17bd52701163338122f1199968f
Steve Herzog <steve.herzog@analog.com>
2022-03-28 08:54:30 -0500

    Fix broken doctest

--- extrude/multi_app.py
+++ extrude/multi_app.py
@@ -132,7 +132,7 @@ def dispatch_child_apps_from_module(root_module: types.ModuleType, configs: dict
     :param root_module: A module that contains one or more child packages to dispatch.
     :param configs: (Optional) See dispatch_child_apps.
 
-    >>> import extrude.examples.example_apps as example_apps
+    >>> import extrude.examples.example_apps_simple as example_apps
 
     >>> dispatch_child_apps_from_module(example_apps)
     """
