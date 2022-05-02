0a3e893f2a53526452a03621735bf7f2355c9024
Valentin Féron <vferon@pentalog.com>
2022-05-02 17:22:04 -0500

    Added support for dispatching dag objects

--- extrude/run_app.py
+++ extrude/run_app.py
@@ -1,6 +1,6 @@
 import json
 from functools import wraps
-from typing import Callable, Iterable
+from typing import Callable, Iterable, Union
 
 import streamlit.bootstrap
 from http2py import HttpClient
@@ -10,29 +10,30 @@ from py2http import mk_app as mk_webservice
 from py2http import run_app as run_webservice
 from py2http.util import run_process
 from streamlitfront.base import dispatch_funcs
+from meshed import DAG
 
 
 # TODO: Maybe split this function in two, by adding mk_app_from_openapi_spec
 def mk_app(
-    funcs: Iterable[Callable] = None,
+    obj: Union[DAG, Iterable[Callable]] = None,
     *,
     api_url: str = None,
     openapi_spec: dict = None,
     func_names: Iterable[str] = None,
     **kwargs
 ):
-    """Generates an front application which will consume a web service exposing a bunch
-    of functions. There are two ways to call this function: from a list of functions or
-    directly from the openapi specification of the web service and a list of function
-    names to pick from the openapi specification
+    """Generates a front application which will consume a web service exposing a bunch
+    of functions. There are three ways to call this function: from a dag, a list of
+    functions or directly from the openapi specification of the web service and a list
+    of function names to pick from the openapi specification.
 
-    :param funcs: A list of functions. Overwrites ``openapi_spec`` and ``func_names``
-    if not None.
-    :param api_url: The base url of the API. Only used if ``funcs`` is not None.
-    :param openapi_spec: A web service openapi specification. Ignored if ``funcs``
+    :param obj: A dag or list of functions. Overwrites ``openapi_spec`` and
+    ``func_names`` if not None.
+    :param api_url: The base url of the API. Only used if ``obj`` is not None.
+    :param openapi_spec: A web service openapi specification. Ignored if ``obj``
     is not None.
     :param func_names: A list of function names to pick from the openapi specification.
-    Ignored if ``funcs`` is not None.
+    Ignored if ``obj`` is not None.
     :param kwargs: Any extra keyword argument used to make the front application.
 
     >>> def foo():
@@ -57,8 +58,13 @@ def mk_app(
 
         return flat_func
 
-    if funcs:
-        ws_config = dict(openapi=dict(base_url=api_url)) if api_url else {}
+    if obj:
+        ws_config = dict(
+            openapi=dict(
+                base_url=api_url
+            )
+        ) if api_url else {}
+        funcs = [fn.func for fn in obj.func_nodes] if isinstance(obj, DAG) else obj
         ws = mk_webservice(funcs, **ws_config)
         openapi_spec = ws.openapi_spec
         func_names = [name_of_obj(func) for func in funcs]
@@ -68,17 +74,18 @@ def mk_app(
     return dispatch_funcs(ws_funcs, **kwargs)
 
 
-def run_app(funcs: Iterable[Callable], *, ws_config: dict = None, **kwargs):
+def run_app(obj: Union[DAG, Iterable[Callable]], *, ws_config: dict = None, **kwargs):
     """Runs a extrude application from a bunch of fucntions. It first run a web service
     application from those functions then runs a front applciation to consume these web
     services
 
-    :param funcs: A list of functions.
+    :param obj: A dag or a list of functions.
     :param ws_config: A predefined configuration for the web service application.
     :param kwargs: Any extra keyword argument used to make the front application.
     """
 
     ws_config = ws_config or {}
+    funcs = [fn.func for fn in obj.func_nodes] if isinstance(obj, DAG) else obj
 
     with run_process(
         func=run_webservice, func_kwargs=dict(app_obj=funcs, **ws_config), is_ready=3,
